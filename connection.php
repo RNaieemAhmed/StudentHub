b7ef18e8a20773959595e3addaa88af310beef2e
Naieem Ahmed <83205993+RNaieemAhmed@users.noreply.github.com>
2022-07-05 19:51:00 +0530

    dbname changes

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
 $servername="dc9qghcgups9m5";
-$username="root";
-$password="";
-$dbname="studenthub";
+$username="stidbcsstqszkj";
+$password="06d5c2cdc22f65f3488b1c70b1f81bc317799c144e13024e1969575329346dae";
+$dbname="dc9qghcgups9m5";
 
 $conn = mysqli_connect($servername,$username,$password,$dbname);
 
